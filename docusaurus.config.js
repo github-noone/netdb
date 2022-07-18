a295e17efed561d7807087fbd843e9227c6cdaf9
No One <noone@MacBook-Pro.home>
2022-07-18 16:12:57 -0400

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'netdb',
-  tagline: 'netdb docs',
+  tagline: 'network docs',
   url: 'https://github.com/github-noone',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -33,7 +33,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
+          routeBasePath: 'netdb',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
